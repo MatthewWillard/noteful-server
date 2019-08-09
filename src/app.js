fc0a691ef8fafde79c2251fa877f8a895d23dc18
Matthew Willard <MWillard85@gmail.com>
2019-08-08 18:19:50 -0600

    more cors fixes

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use('/api/folders', foldersRouter)
 app.use('/api/notes', notesRouter)
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://sheltered-brushlands-82423.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://sheltered-brushlands-82423.herokuapp.com/api"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
